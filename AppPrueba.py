90af9a2d3ce7b0aef1316095c6ed52230f108d56
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-13 21:45:51 -0500

    Update AppPrueba.py

--- AppPrueba.py
+++ AppPrueba.py
@@ -3,5 +3,11 @@ import pandas as pd
 st.title("Busca tu casa")
 st.write("Mi nueva APP")
 buttonL = st.button("Log in")
+buttonI = st.button('Invitado')
+if buttonI:
+  st.write('Estas son las casas')
 if buttonL:
-  st.write('Vivan los paracos')
+  correo = st.text_input("Ingresa por favor tu correo 👇")
+  if correo:
+    st.write("Tu correo es", correo)
+  
