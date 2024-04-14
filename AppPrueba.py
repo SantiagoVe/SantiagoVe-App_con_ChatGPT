a99276391e32c1af3fc8311f71bdbca8fc9153ef
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-13 22:08:39 -0500

    Update AppPrueba.py

--- AppPrueba.py
+++ AppPrueba.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
+
 st.title("Busca tu casa")
 st.write("Mi nueva APP")
+
 buttonL = st.button("Log in")
-buttonI = st.button('Invitado')
-if buttonI:
-  st.write('Estas son las casas')
-if buttonL:  
-  correo = st.text_input("Ingresa tu correo")
-  password = st.text_input("Ingresa tu contraseña", type="password")
-  if st.button("Ingresar"):
-    st.write("Tu correo es: ", correo)
-    st.write("Tu contraseña es: ", password)
-  
+
+if buttonL:
+    correo = st.text_input("Ingresa tu correo")
+    password = st.text_input("Ingresa tu contraseña", type="password")
+    
+    if st.button("Ingresar"):
+        st.write("Tu correo es: ", correo)
+        st.write("Tu contraseña es: ", password)
