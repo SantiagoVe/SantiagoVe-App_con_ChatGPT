19e5921398ffe71810ba11d74a8dc509fb4e75ae
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-13 21:34:16 -0500

    Update AppPrueba.py

--- AppPrueba.py
+++ AppPrueba.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.title("Busca tu casa")
 st.write("Mi nueva APP")
 buttonL = st.button("Log in")
-if button:
+if buttonL:
   correo = st.text_input('Ingresa tu correo')
   password = st.text_input('Ingresa Tu clave')
   st.write('Tu cuenta es: ', correo, password)
