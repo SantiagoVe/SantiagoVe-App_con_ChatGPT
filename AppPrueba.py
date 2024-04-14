ef5688005520ff41d7e044fec7c4ce9fc6ee4a18
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-13 21:36:42 -0500

    Update AppPrueba.py

--- AppPrueba.py
+++ AppPrueba.py
@@ -5,5 +5,4 @@ st.write("Mi nueva APP")
 buttonL = st.button("Log in")
 if buttonL:
   correo = st.text_input('Ingresa tu correo')
-  password = st.text_input('Ingresa Tu clave')
-  st.write('Tu cuenta es: ', correo, password)
+  st.write('Tu correo es ', correo)
