21b152a97a3fa493d215c14f48d03d6ff39383a3
Santiago Velásquez Jaramillo <55595196+SantiagoVe@users.noreply.github.com>
2024-04-13 22:12:50 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,17 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-# Título de la app
-st.title("Mi primera app")
+st.title("Busca tu casa")
+st.write("Mi nueva APP")
 
-# Autor de la app
-st.write("Esta app fue elaborada por Santiago Velasquez")
+buttonL = st.button("Log in")
 
-# Preguntar el nombre al usuario
-nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
-
-# Imprimir mensaje de bienvenida si se proporciona un nombre
-if nombre_usuario:
-    if nombre_usuario == "Angie":
-        st.write("¡¿Ah?! Eres Angie? Te amo mucho mi linda.")
-    else:
-        st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
+if buttonL:
+    correo = st.text_input("Ingresa tu correo")
+    password = st.text_input("Ingresa tu contraseña", type="password")
+    
+    if st.button("Ingresar"):
+        st.write("Tu correo es: ", correo)
+        st.write("Tu contraseña es: ", password)
